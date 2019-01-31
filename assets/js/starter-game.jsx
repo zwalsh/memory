f5fa15df8630cb0adbd941b07d2df79bec9a2625
Zach Walsh <zach.c.walsh@gmail.com>
2019-01-31 17:02:52 -0500

    Finished game

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -12,7 +12,6 @@ class GameBoard extends React.Component {
         this.state = {
             clicks: 0,
             board: this.genBoard(),
-            num_visible: 0,
         };
         console.log("GameBoard made");
     }
@@ -29,9 +28,10 @@ class GameBoard extends React.Component {
                 let c = chars[index];
                 chars = chars.slice(0, index).concat(chars.slice(index + 1));
                 let tile = {
+                    row: i,
+                    col: j,
                     letter: c,
-                    visible: false,
-                    matched: false,
+                    state: "hidden", // state is one of: "hidden", "visible", "matched"
                 };
                 row.push(tile);
             }
@@ -39,23 +39,122 @@ class GameBoard extends React.Component {
         return board;
     }
 
+    // determines if a click at the given index makes a match with a currently-visible tile
+    matchMade(rowIdx, colIdx) {
+        let tile = this.state.board[rowIdx][colIdx];
+        return _.some(this.state.board, (row) => {
+            return _.some(row, (currTile) => {
+               return currTile.state === "visible" &&
+                   !(currTile.row === tile.row && currTile.col === tile.col) &&
+                   currTile.letter === tile.letter;
+            });
+        });
+    }
+
+    // generates the next board, after a click at the given indices
+    boardAfterClick(rowIdx, colIdx) {
+        return _.map(this.state.board, (row) => {
+            return _.map(row, (col) => {
+                return this.nextTile(col, rowIdx, colIdx);
+            });
+        });
+    }
+
+    // generates the next tile, after a click at the given indices
+    nextTile(tile, clickRow, clickCol) {
+        let matched = this.matchMade(clickRow, clickCol);
+        // this tile is currently visible
+        if (tile.state === "visible") {
+            return matched ? _.assign({}, tile, {state: "matched"}) : tile;
+        } else if (tile.row === clickRow && tile.col === clickCol) { // this is the clicked tile
+            let nextState = matched ? "matched" : "visible";
+            return _.assign({}, tile, {state: nextState});
+        } else {
+            return tile;
+        }
+    }
+
+    // returns the number of tiles visible on the given board
+    visibleCount(board) {
+        return _.reduce(board,  (sum, row) => {
+            return sum + _.reduce(row, (sum, tile) => {
+                return sum + (tile.state === "visible" ? 1 : 0);
+            }, 0);
+        }, 0);
+    }
+
+    // flips over all visible tiles
+    clearVisible() {
+        let board = _.map(this.state.board, (row) => {
+            return _.map(row, (tile) => {
+               if (tile.state === "visible") {
+                   return _.assign({}, tile, {state: "hidden"});
+               } else {
+                   return tile;
+               }
+            });
+        });
+        this.setState(_.assign({}, this.state, {board: board}));
+    }
+
+    // handles a click at the given location, changing the state if necessary
+    tileClicked(rowIdx, colIdx) {
+        let visibleCount = this.visibleCount(this.state.board);
+        if (visibleCount >= 2) {
+            return;
+        }
+        let board = this.boardAfterClick(rowIdx, colIdx);
+        visibleCount = this.visibleCount(board);
+        if (visibleCount === 2) {
+            window.setTimeout(this.clearVisible.bind(this), 1000);
+        }
+        this.setState(_.assign({}, this.state, {board: board, clicks: this.state.clicks + 1}));
+    }
+
+    // resets the game and creates a new board
+    reset() {
+        this.setState({board: this.genBoard(), clicks: 0});
+    }
+
     render() {
         let board = _.map(this.state.board, (row, i) => {
             let tiles = _.map(row, (tile, j) => {
-                return <Tile key={i * this.state.board.length + j} tile={tile}/>
+                return <Tile key={i * this.state.board.length + j} tile={tile} clickHandler={() => {
+                    this.tileClicked(i, j);
+                }
+                }/>
             });
             return <div key={100 + i} className="row">{tiles}</div>
         });
 
-        return <div className="container">{board}</div>;
+        return <div>
+            <h1 className="title">Memory Game</h1>
+            <div>
+                <h2 className="title clicks">Clicks: {this.state.clicks}</h2>
+                <Reset clickHandler={this.reset.bind(this)}/>
+            </div>
+            <div className="container">{board}</div>
+        </div>;
     }
 }
 
 function Tile(props) {
-    let tile = props.tile;
-    console.log(props);
-    return <div key={props.key} className="column">
-        {tile.letter}
+    let {tile, clickHandler} = props;
+
+    let content;
+
+    if (tile.state === "hidden") {
+        content = <div className={tile.state} onClick={clickHandler}/>;
+    } else {
+        content = <div className={tile.state}><p>{tile.letter}</p></div>
+    }
+
+    return <div className="column tile">
+        {content}
     </div>;
+}
+
+function Reset({clickHandler}) {
+    return <button onClick={clickHandler} className={"reset"}>Reset</button>;
 }
 
