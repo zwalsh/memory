015c500398cc331d0df945824776b3cda2d27078
Zach Walsh <zach.c.walsh@gmail.com>
2019-02-06 09:07:13 -0500

    Add the GameChannel and update the user socket

--- lib/memory_web/channels/user_socket.ex
+++ lib/memory_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule MemoryWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", MemoryWeb.RoomChannel
+  channel "game:*", MemoryWeb.GameChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
